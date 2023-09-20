14c419dc63e932868b52777ce9e708a175498481
primero <nayalem.arune@ucsp.edu.pe>
2023-09-19 20:35:03 -0500

    p

--- tarea4/10.cpp
+++ tarea4/10.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
 	long limite = 2000000;
-	long long suma = 0; // Cambia a long long para evitar desbordamientos
+	long long suma = 0; 
 	
 	for (int num = 2; num <= limite; ++num) {
 		bool primo = true;
 		
-		for (int i = 2; i * i <= num; ++i) { // Optimización en la comprobación de primos
+		for (int i = 2; i * i <= num; ++i) { 
 			if (num % i == 0) {
 				primo = false;
 				break;
@@ -16,7 +16,7 @@ int main() {
 		}
 		
 		if (primo) {
-			suma += num; // Incrementa la suma por el valor del número primo
+			suma += num; 
 		}
 	}
 	
